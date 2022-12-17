c9c9910092b8dedc7f17e0c9772b51dcc16b3a09
Dakota Bowman <dakotabbowman@outlook.com>
2022-12-17 12:15:28 -0500

    day 1

---
+++ 1_band_name_generator.py
@@ -0,0 +1,5 @@
+# Day 1 - Band Name Generator
+print("Welcome to Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print(f"Your band name could be {city} {pet}")
