5782a12573cd4d32d946495f6bbf7e3aac3bc571
Dakota Bowman <dakotabbowman@outlook.com>
2022-12-17 12:30:46 -0500

    fix spelling of day 5 file

---
+++ 5_password_generator.py
@@ -0,0 +1,13 @@
+# using the for loop with Python lists
+
+# [interactive coding excercise] average height
+
+# [interactive coding excercise] high score
+
+# for loops and the range() function
+
+# [interactive coding excercise] adding even numbers
+
+# [interactive coding excercise] the fizzbuzz job interview question
+
+# Create A Password Generator
