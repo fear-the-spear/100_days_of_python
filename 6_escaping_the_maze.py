eb603fbdcea07128c61d8324c3fcbdd71fa7e3f1
Dakota Bowman <dakotabbowman@outlook.com>
2022-12-18 23:17:41 -0500

    day 6 template

---
+++ 6_escaping_the_maze.py
@@ -0,0 +1,13 @@
+# defining and calling python functions
+
+# the hurdles loop challenge
+
+# indentation in python
+
+# while loops
+
+# hurdles challenge usings while loops
+
+# jumping over hurdles with varialbe heights
+
+# escaping the maze
