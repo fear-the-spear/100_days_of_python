9fa700d7e30fa088dc4297b9c22947d577d1a2ca
Dakota Bowman <dakotabbowman@outlook.com>
2022-12-30 22:16:06 -0500

    day 8 pt.2 - complete caesar cipher

--- 8_caesar_cipher.py
+++ 8_caesar_cipher.py
@@ -1,4 +1,5 @@
 # functions with inputs
+from art import logo
 import math
 function = '''
 def my_function(something): <-- 'something' is the parameter
@@ -66,3 +67,141 @@ prime_checker(number=n)
 # NOTE: ALL THIS DOES IS CHECK FOR NUMBERS BETWEEN THE CHECKED NUMBER
 #       AND 2 TO SEE IF ANY OF THEM GO INTO THE CHECKED NUMBERS EVENLY.
 #       IF ANY OF THEM DO, THAT MEANS IT'S NOT PRIME.
+
+# caesar cipher part 1 - encryption
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+# TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+
+def encrypt(encrypt, text):
+    # TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+    encoded_message = ""
+    for letter in text:
+        shifted_text = alphabet.index(letter) + shift
+        encoded_message += alphabet[shifted_text]
+    print(f"The encoded text is {encoded_message}")
+
+    # e.g.
+    # plain_text = "hello"
+    # shift = 5
+    # cipher_text = "mjqqt"
+    # print output: "The encoded text is mjqqt"
+
+    # HINT: How do you get the index of an item in a list:
+    # https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    # 🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+
+# TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+encrypt(encrypt, text)
+# caesar cipher part 2 - decryption
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        cipher_text += alphabet[new_position]
+    print(f"The encoded text is {cipher_text}")
+
+
+# TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        cipher_text += alphabet[new_position]
+    print(f"The decoded text is {cipher_text}")
+
+    # TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
+    # e.g.
+    # cipher_text = "mjqqt"
+    # shift = 5
+    # plain_text = "hello"
+    # print output: "The decoded text is hello"
+
+
+# TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+else:
+    decrypt(plain_text=text, shift_amount=shift)
+
+
+# caesar cipher part 3 - reorganizing our code
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+            'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def caesar(start_text, shift_amt, dir):
+    end_text = ""
+    for letter in start_text:
+        position = alphabet.index(letter)
+        if dir == "encode":
+            shift_amt *= -1
+        new_position = position + shift_amt
+        end_text += alphabet[new_position]
+    print(f"The {dir}d text is {end_text}")
+
+
+caesar(start_text=text, shift_amt=shift, dir=direction)
+
+# caesar cipher part 4 - user experience improvements & final touches
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+            'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+
+print(logo)
+
+is_running = "yes"
+while is_running == "yes":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    is_running = input(
+        "Type 'yes' if you want to go again. Otherwise, type 'no'.\n")
