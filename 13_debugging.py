e19364f7fd10a7c11674b77690f45703a0119e5a
Dakota Bowman <dakotabbowman@outlook.com>
2023-01-23 22:23:22 -0500

    day 13 - full code

---
+++ 13_debugging.py
@@ -0,0 +1,6 @@
+# I'm only keeping this file here for semantic purposes.
+# The instructor shared debugging tips, and had the students
+#   debug code that we had already written - except she
+#   modified little bits of the code to create bugs.
+
+# So, there is no code or project to be written here.
