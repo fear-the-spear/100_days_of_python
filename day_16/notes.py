6d9141de92d696fe924a1870f96f630ecb117f21
Dakota Bowman <dakotabbowman@outlook.com>
2023-01-23 22:30:45 -0500

    day 16 - projects are now using multiple files, so every day from here on out will have a directory rather than just a file (or random files in root dir.)

---
+++ day_16/notes.py
@@ -0,0 +1,17 @@
+# Up to this point, we have been using proceedural programming
+# to write our code. Proceedural Programming is the process of
+# setting up proceedurs or functions that do particular things.
+# For the most part, the code runs top to bottom and then jumps
+# out into a function as needed.
+
+# object-oriented programming is the combining of pieces of data (attributes)
+#  and some functionality (methods)
+# these objects are created using blueprints (classes)
+
+# the virtual restaurant
+# waiter - two things we need to think about:
+#   1. what it has (attributes)
+#   2. what it does (methods)
+
+# an attribute is a variable that's associated with a modeled object
+# a method is a function that a particular modeled object can do.
