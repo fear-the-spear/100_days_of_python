0b4f0f0909c957d8a419df43885eb2550f94131a
Dakota Bowman <dakotabbowman@outlook.com>
2023-03-06 19:44:22 -0500

    day 25 - all code for us states guessing game

---
+++ days_21-30/day_25/us_states_game/main.py
@@ -0,0 +1,33 @@
+import pandas
+import turtle
+
+screen = turtle.Screen()
+screen.title("US States Game")
+img = "blank_states_img.gif"
+screen.addshape(img)
+turtle.shape(img)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer = screen.textinput(
+        title=f"{len(guessed_states)}/50 States Correct", prompt="Guess A State!").title()
+
+    if answer == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer in all_states:
+        guessed_states.append(answer)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())  # <- grabs first element in data
