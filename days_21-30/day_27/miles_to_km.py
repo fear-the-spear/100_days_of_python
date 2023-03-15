048b9c8f7c9b8095ea5754ce651187cdc0f8a485
Dakota Bowman <dakotabbowman@outlook.com>
2023-03-15 14:40:53 -0400

    full miles-to-km calculator, rounded to the nearest hundredth place

--- days_21-30/day_27/miles_to_km.py
+++ days_21-30/day_27/miles_to_km.py
@@ -1,1 +1,32 @@
 import tkinter
+
+window = tkinter.Tk()
+window.title("Miles To Kilometers Converter")
+window.config(padx=20, pady=20)
+
+
+def calculate():
+    miles_data = float(input.get())
+    km_data = round(miles_data * 1.60934, 2)
+    km_result_label.config(text=f"{km_data}")
+
+
+is_equal_label = tkinter.Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
+
+miles_label = tkinter.Label(text="miles")
+miles_label.grid(column=2, row=0)
+
+km_label = tkinter.Label(text="km")
+km_label.grid(column=2, row=1)
+
+km_result_label = tkinter.Label(text=0)
+km_result_label.grid(column=1, row=1)
+
+input = tkinter.Entry(width=8)
+input.insert(index=2, string="0")
+input.grid(column=1, row=0)
+
+calc_button = tkinter.Button(text="Calculate", command=calculate)
+calc_button.grid(column=1, row=2)
+window.mainloop()
