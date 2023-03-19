518a19804e2edfa9b2364d286529fba7f47f51c3
Dakota Bowman <lildboyo@gmail.com>
2023-03-18 23:44:20 -0400

    add functionality for hitting the start button and going through the increments of time in proper order

--- days_21-30/day_28/pomodoro_app/main.py
+++ days_21-30/day_28/pomodoro_app/main.py
@@ -1,4 +1,5 @@
 import tkinter
+import math
 # ------------------------------- CONSTANTS ---------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,11 +9,44 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 # ------------------------------ TIMER RESET --------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title.config(text="Work", fg=GREEN)
 # -------------------------- COUNTDOWN MECHANISM ----------------------------- #
+
+
+def count_down(count):
+    count_min = math.floor(count / 60)
+    if count_min < 10:
+        count_min = f"0{count_min}"
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1, count_down, count - 1)
+    else:
+        start_timer()
 
 
 # -------------------------------- UI SETUP ---------------------------------- #
@@ -29,13 +63,13 @@ canvas = tkinter.Canvas(width=200, height=224,
                         bg=YELLOW, highlightthickness=0)
 tomato_img = tkinter.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white",
-                   font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white",
+                                font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 # START BUTTON
 start_button = tkinter.Button(
-    text="Start", highlightbackground=YELLOW, width=2)
+    text="Start", highlightbackground=YELLOW, width=2, command=start_timer)
 start_button.grid(column=0, row=2)
 
 # RESET BUTTON
@@ -44,7 +78,7 @@ reset_button = tkinter.Button(
 reset_button.grid(column=2, row=2)
 
 # CHECK BOX
-check_box = tkinter.Label(text=" ✔︎ ", fg=GREEN, bg=YELLOW)
-check_box.grid(column=1, row=3)
+check_marks = tkinter.Label(text=" ✔︎ ", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 window.mainloop()
