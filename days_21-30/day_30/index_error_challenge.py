b796b73a5de8f0c1032652a5fd456279a092a947
Dakota Bowman <lildboyo@gmail.com>
2023-03-26 15:24:27 -0400

    index error and key error challenges

---
+++ days_21-30/day_30/index_error_challenge.py
@@ -0,0 +1,16 @@
+import random
+fruits = ["Apple", "Pear", "Orange"]
+
+# TODO: Catch the exception and make sure the code runs without crashing.
+
+
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+    except IndexError:
+        print("Fruit pie")
+    else:
+        print(fruit + " pie")
+
+
+make_pie(4)
