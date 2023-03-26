c518f43f4023e4fbec8e899d8de388740444c674
Dakota Bowman <lildboyo@gmail.com>
2023-03-26 15:24:03 -0400

    notes on error handling

---
+++ days_21-30/day_30/error_handling.py
@@ -0,0 +1,25 @@
+# Types of Errors
+
+# FileNotFound
+# with open("a_file.txt") as file:
+#     file.read()
+
+# KeyError
+# a_dict = {"key": "value"}
+# value = a_dict["non_existent_key"]
+
+# IndexError
+# fruit_list = ["Apple", "Banana", "Pear"]
+# fruit = fruit_list[3]
+
+# TypeError
+# text = "abc"
+# print(text + 5)
+
+# Error Handling
+
+# There are 4 keywords that are really important when it comes to error handling
+# 1. try: when executing something that might cause an exception
+# 2. except: do this if exception occurs
+# 3. else: do this if there were no exceptions
+# 4. finally: do this no matter what happens
