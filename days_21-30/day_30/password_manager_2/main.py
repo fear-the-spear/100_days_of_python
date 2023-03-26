4b9c1a28a3c11059b377ba649b2be98638ceaa2d
Dakota Bowman <lildboyo@gmail.com>
2023-03-26 15:31:57 -0400

    update to the password manager project, add exception handling

---
+++ days_21-30/day_30/password_manager_2/main.py
@@ -0,0 +1,138 @@
+from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import json
+# --------------------------- PASSWORD GENERATOR ----------------------------- #
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+password_list = []
+
+
+def gen_pass():
+    '''
+    Generates a password from a combination of 8-10 letters,
+    2-4 numbers, and 2-4 symbols. It then copies the password
+    to the clipboard for easy pasting into a web form.'''
+    ltrs = [choice(letters) for _ in range(randint(8, 10))]
+    smbls = [choice(symbols) for _ in range(randint(2, 4))]
+    nmbrs = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = ltrs + smbls + nmbrs
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+
+    passwd_entry.delete(0, END)
+    passwd_entry.insert(0, password)
+    passwd_entry.clipboard_clear()
+    passwd_entry.clipboard_append(passwd_entry.get())
+# ----------------------------- SAVE PASSWORD -------------------------------- #
+
+
+def save():
+    '''
+    'Save' button-press functionality:
+
+    Saves user-entered data from all data fields into a
+    file called 'data.txt' - unless one or more fields is empty.
+    '''
+    web_addr = web_entry.get().lower()
+    email = user_entry.get()
+    passwd = passwd_entry.get()
+    new_data = {
+        web_addr: {
+            "email": email,
+            "password": passwd
+        }
+    }
+
+    if len(web_addr) == 0 or len(passwd) == 0:
+        messagebox.showinfo(
+            title="Oops!", message="Please don't leave any fields empty!")
+    else:
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            web_entry.delete(0, END)
+            passwd_entry.delete(0, END)
+
+# -------------------------------- SEARCH ------------------------------------ #
+
+
+def search():
+    search_result = web_entry.get()
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showerror(title="ERROR", message="File Not Found!")
+    else:
+        if search_result in data:
+            email = data[search_result]["email"]
+            password = data[search_result]["password"]
+            messagebox.showinfo(
+                title=search_result, message=f"Website: {search_result}\n\nEmail:\n{email}\nPassword:\n{password}")
+        else:
+            messagebox.showinfo(
+                title="No Data Available", message=f"There is no saved data for {search_result}"
+            )
+
+
+# ------------------------------- UI SETUP ----------------------------------- #
+# Window Setup
+window = Tk()
+window.title("Password Manager")
+window.config(padx=75, pady=75)
+
+# Canvas Setup & Background Image
+canvas = Canvas(width=200, height=200)
+lock_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(column=1, row=0)
+
+# Labels
+web_label = Label(text="Website")
+web_label.grid(column=0, row=1)
+user_label = Label(text="Email/Username")
+user_label.grid(column=0, row=2)
+passwd_label = Label(text="Password")
+passwd_label.grid(column=0, row=3)
+
+# Text Boxex
+web_entry = Entry(width=21)
+web_entry.grid(column=1, row=1)
+user_entry = Entry(width=37)
+user_entry.grid(column=1, row=2, columnspan=2)
+user_entry.insert(END, "dakotabbowman@outlook.com")
+passwd_entry = Entry(width=21)
+passwd_entry.grid(column=1, row=3)
+
+# Search Button
+search_btn = Button(text="Search", width=11, command=search)
+search_btn.grid(column=2, row=1)
+
+# Generate Password Button
+generate_btn = Button(text="Generate Password", width=11, command=gen_pass)
+generate_btn.grid(column=2, row=3)
+
+# Add Button
+add_btn = Button(text="Add", width=35, command=save)
+add_btn.grid(column=1, row=4, columnspan=2)
+
+window.mainloop()
