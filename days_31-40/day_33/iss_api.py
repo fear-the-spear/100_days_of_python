28e803abbe56d1868ce63ae13dc7b3500f852d9e
Dakota Bowman <lildboyo@gmail.com>
2023-04-08 10:08:31 -0400

    simple program that fetches current position of the international space station (longitude and latitude) and prints it to the terminal

---
+++ days_31-40/day_33/iss_api.py
@@ -0,0 +1,15 @@
+import requests
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+
+data = response.json()["iss_position"]
+lon = data["longitude"]
+lat = data["latitude"]
+
+iss_position = (
+    lon,
+    lat
+)
+
+print(iss_position)
